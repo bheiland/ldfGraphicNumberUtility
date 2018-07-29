f1a17ae88d7a98145b3cfebebe9c9f33e6ede199
Brian Heiland <HeilB0LW@prscoad.com>
2018-07-29 07:05:34 -0400

    Added Code and button to clear dictionary and table so a different dictionary can be loaded.

--- LDFFileReadWrite1/Form1.Designer.vb
+++ LDFFileReadWrite1/Form1.Designer.vb
@@ -51,6 +51,7 @@ Partial Class Form1
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumericUpDownLineToReplace, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumericUpDownGraphicLineToScan, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -302,11 +303,21 @@ Partial Class Form1
         Me.Label1.TabIndex = 47
         Me.Label1.Text = "Global font for characters > 255"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(490, 36)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(231, 23)
+        Me.Button1.TabIndex = 48
+        Me.Button1.Text = "Clear Dictionary File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1147, 666)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox3)
@@ -372,5 +383,5 @@ Partial Class Form1
     Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
-
+    Friend WithEvents Button1 As Button
 End Class
--- LDFFileReadWrite1/Form1.vb
+++ LDFFileReadWrite1/Form1.vb
@@ -1484,5 +1484,8 @@ Public Class Form1
 
     End Sub
 
-
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        lookupDict.Clear()
+        ListViewDictionary.Items.Clear()
+    End Sub
 End Class
